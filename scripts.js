3bd2d73603041f8780a9712bd05cb0468a527e57
Akhil chandra <akhilchandra2309@gmail.com>
2022-12-27 11:09:14 +0530

    update

--- scripts.js
+++ scripts.js
@@ -9,16 +9,16 @@ let secondsHand = document.querySelector(".second-hand")
 
 function clock() {
     const now = new Date();
-    let seconds = now.getSeconds();
+    const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90 + ((seconds/60) * 6);
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hourDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+    const hourDegrees = ((hours / 12) * 360) + 90+ ((minutes / 60) * 30)
     hoursHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
    
